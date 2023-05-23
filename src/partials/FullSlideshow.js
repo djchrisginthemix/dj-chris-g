cf560fa3ae8de0807225f1f87210c449c4822a28
tjhammer845 <tomh@laseraway.net>
2023-05-23 01:41:40 -0400

    done for today

--- src/partials/FullSlideshow.js
+++ src/partials/FullSlideshow.js
@@ -1,27 +1,24 @@
 import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
-import { ParallaxProvider, Parallax } from 'react-scroll-parallax';
-import djcgSlide1 from "../assets/images/djcgSlide1.png";
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import { ParallaxProvider } from 'react-scroll-parallax'
+import djcgSlide1 from '../assets/images/djcgSlide1.png'
 
 const slides = [
   {
     image: djcgSlide1,
-    alt: "Slide 1",
-    parallaxStrength: 0.3
+    alt: 'Slide 1'
   },
   {
-    image: "https://via.placeholder.com/1200x800?text=Slide+2",
-    alt: "Slide 2",
-    parallaxStrength: 0.5
+    image: 'https://via.placeholder.com/1200x800?text=Slide+2',
+    alt: 'Slide 2'
   },
   {
-    image: "https://via.placeholder.com/1200x800?text=Slide+3",
-    alt: "Slide 3",
-    parallaxStrength: 0.7
+    image: 'https://via.placeholder.com/1200x800?text=Slide+3',
+    alt: 'Slide 3'
   }
-];
+]
 
 const FullSlideshow = () => {
   const settings = {
@@ -32,47 +29,33 @@ const FullSlideshow = () => {
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 3000,
-    fade: true,
-  };
+    fade: true
+  }
+
 
   return (
-    <div className="djcg-slideshow relative w-full max-w8xl">
-      <ParallaxProvider>
-        <Parallax y={[-20, 20]} tagOuter="div">
-          <div className="parallax-container">
+      <ParallaxProvider scrollAxis="vertical">
+      <div className='djcg-slideshow w-screen h-screen'>
+          <div className='parallax-container'>
             <Slider {...settings}>
               {slides.map((slide, index) => (
-                <div key={index} className="slide">
+                <div key={index} className='slide'>
                   <div
-                    className="parallax-slide"
-                    style={{
-                      height: '100vh',
-                      position: 'relative',
-                      overflow: 'hidden'
-                    }}
+                    className='parallax-slide'
                   >
                     <img
                       src={slide.image}
                       alt={slide.alt}
-                      className="w-full h-full object-cover"
-                      style={{
-                        position: 'absolute',
-                        top: '50%',
-                        left: '50%',
-                        transform: 'translate(-50%, -50%)',
-                        maxHeight: '100%',
-                        maxWidth: '100%',
-                      }}
+                      className='w-full h-full object-cover'
                     />
                   </div>
                 </div>
               ))}
             </Slider>
           </div>
-        </Parallax>
-      </ParallaxProvider>
-    </div>
-  );
-};
+      </div>
+    </ParallaxProvider>
+  )
+}
 
-export default FullSlideshow;
+export default FullSlideshow
