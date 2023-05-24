a7576802d531de520dab6b8cc478cd3b168d3df3
tjhammer845 <tomh@laseraway.net>
2023-05-23 23:02:46 -0400

    done

--- src/components/LinktreeSubscribe.js
+++ src/components/LinktreeSubscribe.js
@@ -6,7 +6,7 @@ function LinktreeSubscribeButton() {
       href="https://linktr.ee/djchrisginthemix"
       target="_blank"
       rel="noopener noreferrer"
-      className="relative text-white font-bold w-40 h-40 flex items-center justify-center rounded-full transition duration-500 ease-in-out hover:bg-blue-ice"
+      className="relative font-headline text-white font-bold w-40 h-40 flex items-center justify-center rounded-full transition duration-500 ease-in-out hover:bg-blue-ice"
     >
       Follow
     </a>
